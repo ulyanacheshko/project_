5cda599052210c2f88d2cb7bc8870c6cfa17ebf7
Ulyana Cheshko <ylyanacheshko@gmail.com>
2022-09-24 13:01:26 +0300

    in.txt

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.io.*;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
+        try (FileInputStream in = new FileInputStream("C:\\Users\\Юзер\\IdeaProjects\\project_\\src\\in.txt");
+             FileOutputStream os = new FileOutputStream("C:\\Users\\Юзер\\IdeaProjects\\project_\\src\\out.txt")) {
+            byte[] buffer = new byte[in.available()];
+            in.read(buffer, 0, buffer.length);
+            os.write(buffer, 0, buffer.length);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
